cea8e9cbe8ccaff9788453b1e7150b42ddb42f5d
kaeppen <seho@itu.dk>
2021-10-03 21:15:14 +0200

    Færdiggør client

--- client/client.go
+++ client/client.go
@@ -4,9 +4,30 @@ import (
 	"fmt"
 	"net/http"
 	"io"
+	"time"
+	"bytes"
 )
 
 func main() {
+	fmt.Println("Now demoing a GET request")
+	fmt.Println(getDemo())
+	fmt.Println("---------------")
+	time.Sleep(1*time.Second)
+	fmt.Println("Now demoing a POST request")
+	fmt.Println(postDemo())
+	fmt.Println("---------------")
+	time.Sleep(1*time.Second)
+	fmt.Println("Now demoing a DELETE request")
+	fmt.Println(deleteDemo())
+	fmt.Println("---------------")
+	time.Sleep(1*time.Second)
+	fmt.Println("Now demoing a PUT request")
+	fmt.Println(putDemo())
+	
+}
+
+
+func getDemo() string {
 	resp, err := http.Get("http://localhost:8080/courses")
 	if err != nil {
 		fmt.Println(err)
@@ -14,5 +35,73 @@ func main() {
 	defer resp.Body.Close() 
 	body, err := io.ReadAll(resp.Body)
 	bodyString := string(body)
-	fmt.Println(bodyString)
+	return bodyString
+
+}
+
+func postDemo() string {
+	client := &http.Client{}
+	var jsonStr = []byte(`{"id": "5", "rating": 0.1, "name": "Bæredygtigt design", "students": []}`)
+	//make request
+	req, err := http.NewRequest(http.MethodPost, "http://localhost:8080/courses", bytes.NewBuffer(jsonStr)) //send the json string as payload
+	req.Header.Set("X-Custom-Header", "myvalue") //wtf
+	req.Header.Set("Content-Type", "application/json")
+	if err != nil {
+		fmt.Println(err)
+	}
+	//fetch request
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+		return err.Error()
+	}
+	defer resp.Body.Close()
+	//read response 
+	body, err := io.ReadAll(resp.Body)
+	bodyString := string(body)
+	return bodyString
+}
+
+func deleteDemo() string {
+	client := &http.Client{}
+	//make request
+	req, err := http.NewRequest(http.MethodDelete, "http://localhost:8080/courses/1", nil)
+	if err != nil {
+		fmt.Println(err)
+	}
+	//fetch request
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+		return err.Error()
+	}
+	defer resp.Body.Close()
+	//read response 
+	body, err := io.ReadAll(resp.Body)
+	bodyString := string(body)
+	return bodyString
+}
+
+//does not output anything atm
+func putDemo() string {
+	client := &http.Client{}
+	var jsonStr = []byte(`{"id": "4", "rating": 9.5, "name": "Algo", "students": ["Alle", "Er med"]}`)
+	//make request
+	req, err := http.NewRequest(http.MethodPut, "http://localhost:8080/courses/2", bytes.NewBuffer(jsonStr)) //send the json string as payload
+	//req.Header.Set("X-Custom-Header", "myvalue") //wtf
+	req.Header.Set("Content-Type", "application/json")
+	if err != nil {
+		fmt.Println(err)
+	}
+	//fetch request
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+		return err.Error()
+	}
+	defer resp.Body.Close()
+	//read response 
+	body, err := io.ReadAll(resp.Body)
+	bodyString := string(body)
+	return bodyString
 }
--- main.go
+++ main.go
@@ -2,12 +2,27 @@ package main
 
 import (
 	"net/http"
-
+	"fmt"
 	"github.com/gin-gonic/gin"
 )
 
 //hvis man virkelig er fresh, så skal nogle af felterne i struct være pointers, men det er ikke i denne opgaves scope
 
+//THESE STRUCTS ARE JUST FOR SHOW 
+type teacher struct {
+	ID      string   `json:"id"`
+	Name    string   `json:"name"`
+	Popularity float64 `json: "popularity"`
+}
+
+type student struct {
+	ID      string   `json:"id"`
+	Name    string   `json:"name"`
+	Courses []string `json:"courses"` 
+}
+//-----------------------
+
+//TODO: mangler at lave workload... 
 type course struct {
 	ID       string   `json:"id"`
 	Rating   float64  `json:"rating"`
@@ -45,7 +60,8 @@ func addCourse(c *gin.Context) {
 
 	// Add the new course to the slice.
 	courses = append(courses, newCourse)
-	c.IndentedJSON(http.StatusCreated, newCourse)
+	c.IndentedJSON(http.StatusCreated, gin.H{"Course":newCourse, "message":"successfully added"}) 
+	return
 }
 
 func getCourseByID(c *gin.Context) {
@@ -61,11 +77,12 @@ func getCourseByID(c *gin.Context) {
 
 func deleteCourseByID(c *gin.Context) {
 	id := c.Param("id")
+	var deleted course
 	var oldLength = len(courses) //get the length of the array before (to be used when determining status)
 	var newArray []course
 	for i := 0; i < len(courses); i++ {
 		if courses[i].ID == id {
-			//do nothing
+			deleted = courses[i]
 		} else {
 			//append to the new slice
 			newArray = append(newArray, courses[i])
@@ -78,8 +95,10 @@ func deleteCourseByID(c *gin.Context) {
 	//same length = nothing deleted :(
 	if len(courses) == oldLength {
 		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Course not found"})
+		return
 	} else {
-		c.IndentedJSON(http.StatusOK, gin.H{"message": "Course deleted succesfully"})
+		c.IndentedJSON(http.StatusOK, gin.H{"Course": deleted, "message":"successfully deleted"})
+		return
 	}
 }
 
@@ -89,7 +108,8 @@ func updateCourse(c *gin.Context) {
 
 	//bind the recieved json to the new object
 	if err := c.BindJSON(&newCourse); err != nil {
-		return
+		fmt.Println(err.Error())
+		return 
 	}
 
 	//iterate over all courses and replace the one in question
@@ -98,9 +118,10 @@ func updateCourse(c *gin.Context) {
 		if courses[i].ID == id {
 			courses[i] = newCourse
 			//all good
-			c.IndentedJSON(http.StatusOK, newCourse)
+			c.IndentedJSON(http.StatusOK, gin.H{"Course": newCourse, "message": "successfully updated"})
 			return
 		}
 	}
 	c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "something went wrong"})
+	return 
 }
